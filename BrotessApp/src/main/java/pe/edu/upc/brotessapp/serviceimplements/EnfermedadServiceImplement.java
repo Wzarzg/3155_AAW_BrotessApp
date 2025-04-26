811232e44bbd5d2655cefe73309d3322e19d1880
DAlescitaMelina <u20211g015@upc.edu.pe>
2025-04-26 18:20:56 -0500

    add enfermedades

---
+++ BrotessApp/src/main/java/pe/edu/upc/brotessapp/serviceimplements/EnfermedadServiceImplement.java
@@ -0,0 +1,34 @@
+package pe.edu.upc.brotessapp.serviceimplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.brotessapp.entities.Enfermedad;
+import pe.edu.upc.brotessapp.repositories.IEnfermedadRepository;
+import pe.edu.upc.brotessapp.serviceinterfaces.IEnfermedadService;
+
+import java.util.List;
+@Service
+public class EnfermedadServiceImplement implements IEnfermedadService {
+    @Autowired
+    private IEnfermedadRepository eR;
+
+    @Override
+    public List<Enfermedad> list() {
+        return eR.findAll();
+    }
+
+    @Override
+    public void insert(Enfermedad e) {
+        eR.save(e);
+    }
+
+    @Override
+    public Enfermedad listId(int id) {
+        return eR.findById(id).orElse(new Enfermedad());
+    }
+
+    @Override
+    public void update(Enfermedad e) {
+        eR.save(e);
+    }
+}
