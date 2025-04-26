1451896dbff172bf7dcbe72f930e7e1e0354c3eb
tony44405 <catalinapropvp@gmail.com>
2025-04-25 21:16:45 -0500

    Add files via upload

---
+++ BrotessApp/src/main/java/pe/edu/upc/brotessapp/serviceinterfaces/INotificacionService.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.brotessapp.serviceinterfaces;
+
+
+
+import pe.edu.upc.brotessapp.entities.Notificacion;
+
+import java.util.List;
+
+public interface INotificacionService {
+    public List<Notificacion> list();
+    public void insert(Notificacion n);
+    public Notificacion listId(int id);
+    public void update(Notificacion n);
+}
